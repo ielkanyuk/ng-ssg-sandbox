3c877c1cefee1186c6175b77ca4578d6917569ab
ielkanyuk <ielkanyuk@gmail.com>
2021-09-10 22:46:20 +0500

    Add Scully.io

---
+++ scully.config.ts
@@ -0,0 +1,40 @@
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { getSitemapPlugin } from '@gammastream/scully-plugin-sitemap';
+
+const SitemapPlugin = getSitemapPlugin();
+setPluginConfig(SitemapPlugin, {
+  urlPrefix: 'https://example.com',
+  sitemapFilename: 'sitemap.xml',
+  changeFreq: 'daily',
+  priority: [
+    '1.0',
+    '0.8',
+  ],
+  ignoredRoutes: ['/404']
+});
+
+export const config: ScullyConfig = {
+  projectRoot: './src',
+  projectName: 'example.com',
+  outDir: './dist/static', // directory for scully build artifacts
+  defaultPostRenderers: [],
+  routes: {},
+  extraRoutes: [
+  ],
+  puppeteerLaunchOptions: {
+    args: [
+      '--disable-gpu',
+      '--renderer',
+      '--no-sandbox',
+      '--no-service-autorun',
+      '--no-experiments',
+      '--no-default-browser-check',
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-extensions'
+    ]
+  }
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ScullyLibModule } from '@scullyio/ng-lib';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    ScullyLibModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
